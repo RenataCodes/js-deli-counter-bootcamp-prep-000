183121484e4029e142cee0cc9ae6226578d8ebd7
RenataCodes <psych.coaching@gmail.com>
2017-09-27 18:34:58 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 var katzDeliLine = [];
 
 function takeANumber(katzDeliLine, name) {
-  var position = katzDeliLine.length-1;
-  console.log(`Welcome, ${name}. You are number ${position} in line.`)
-}
+  katzDeliLine.push(name);
+  if (katzDeliLine.length == 1) {
+    console.log(`Welcome, ${name}. You are number 1 in line.`)
+  } else {
+    var position = katzDeliLine.length + 1;
+    console.log(`Welcome, ${name}. You are number ${position} in line.`)
+  }
